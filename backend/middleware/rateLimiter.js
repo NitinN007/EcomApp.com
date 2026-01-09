af70a52b0b7473d8d5b5e2385664a9d711f3935f
Nitin Negi <nitinnegi5062@gmail.com>
2026-01-09 14:37:50 +0530

    rate limiter

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,12 +1,9 @@
 const rateLimit = require("express-rate-limit");
 
-exports.apiLimiter =
-    process.env.NODE_ENV === "production"
-        ? rateLimit({
-          windowMs: 15 * 60 * 1000,
-          max: 100,
-          standardHeaders: true,
-          legacyHeaders: false,
-          message: "Too many requests, try again later",
-        })
-        : (req, res, next) => next(); // 🔥 NO LIMIT IN DEV
+exports.apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per window
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: "Too many requests, try again later",
+});
