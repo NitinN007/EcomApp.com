36c462f3fff2a9ddc83d8feadeb5ff2ad4060279
Nitin Negi <nitinnegi5062@gmail.com>
2026-01-09 14:48:57 +0530

    rate limiter added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
 const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
+const { rateLimiter } = require("../middleware/rateLimiter");
+router.post("/register",rateLimiter,register);
+router.post("/login", rateLimiter,login);
 
 module.exports = router;
