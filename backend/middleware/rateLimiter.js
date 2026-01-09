66a5709f478839a285f074042f22e9640e398317
Nitin Negi <nitinnegi5062@gmail.com>
2026-01-09 14:53:26 +0530

    rate limiter added

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,10 +1,9 @@
 const rateLimit = require("express-rate-limit");
 
-exports.apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per window
+exports.rateLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
     standardHeaders: true,
     legacyHeaders: false,
     message: "Too many requests, try again later",
-
 });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
-const router = express.Router();
 const { rateLimiter } = require("../middleware/rateLimiter");
-router.post("/register",rateLimiter,register);
-router.post("/login", rateLimiter,login);
+
+const router = express.Router();
+
+router.post("/register", rateLimiter, register);
+router.post("/login", rateLimiter, login);
 
 module.exports = router;
